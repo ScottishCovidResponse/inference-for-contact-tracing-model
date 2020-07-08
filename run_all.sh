aeb998786abac309ec76c8f401aa6374478745c9
Rikiya TAKAHASHI <Rikiya.Takahashi@gmail.com>
2020-07-08 10:59:12 +0100

    Bugfix on slurm scripts.

--- run_all.sh
+++ run_all.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-# Settings
+# Settings: an example below gives a sensitivity analysis based on 
+# 10*100=1000 random pairs of input parameters (e.g., disease parameters) and output metrics (e.g., total n_deaths)
 n_tasks=10
 n_simulations_per_task=100
-workdir=${HOME}/covid-19/trial
+workdir=${HOME}/covid-19/sensitivity1000
 
-# Main jobs
-sbatch --wait --array=0-${n_tasks}:1 slurm_draw_parameters ${workdir} ${n_simulations_per_task}
+# Parallel sampling of parameters and results
+sbatch --wait --array=1-${n_tasks}:1 slurm_draw_parameters ${workdir} ${n_simulations_per_task}
 
+# Unifying the results into CSV files
 dirlistfile=${workdir}/workdirlist.txt
 rm -f ${dirlistfile}
+txtlist=`find ${workdir} -name workdirlist.txt`
 touch ${dirlistfile}
-ls ${workdir}/job_*_* > ${dirlistfile}
-
+for txt in ${txtlist}
+do
+	cat ${txt} >> ${dirlistfile}
+done
 python src/unify_draws.py ${dirlistfile} ${workdir}
 
+# Draw SHAP plots for the final sensitivity analysis
 python src/analyse_sensitivity.py ${workdir} ${workdir}
-
