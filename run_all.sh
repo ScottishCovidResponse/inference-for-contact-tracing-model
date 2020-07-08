db946cc87e827e79cd57649e78d8481054607129
Rikiya TAKAHASHI <Rikiya.Takahashi@gmail.com>
2020-07-08 10:08:43 +0100

    Fixed slurm scripts.

---
+++ run_all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Settings
+n_tasks=10
+n_simulations_per_task=100
+workdir=${HOME}/covid-19/trial
+
+# Main jobs
+sbatch --array=0-${n_tasks}:1 slurm_draw_parameters ${workdir} ${n_simulations_per_task}
+
+dirlistfile=${workdir}/workdirlist.txt
+rm -f ${dirlistfile}
+ls ${workdir}/job_*_* > ${dirlistfile}
+
+python src/unify_draws.py ${dirlistfile} ${workdir}
+
+python src/analyse_sensitivity.py ${workdir} ${workdir}
+
